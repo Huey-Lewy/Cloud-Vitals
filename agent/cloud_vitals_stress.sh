14e7e272c87255f30f894d9ee67bbf0d160e83b1
mynor <mynormend@gmail.com>
2025-07-19 16:27:31 -0400

    Changed the cloud_vitals_stress.sh script to make io and net to work properly

--- agent/cloud_vitals_stress.sh
+++ agent/cloud_vitals_stress.sh
@@ -1,3 +1,4 @@
+  GNU nano 7.2                                 cloud_vitals_stress.sh
 #!/usr/bin/env bash
 # cloud_vitals_stress.sh
 # Note: Make sure this is is executable with chmod +x
@@ -14,9 +15,12 @@ case "$CLASS" in
     ARGS=(--iomix 4) ;;
   filesystem)
     ARGS=(--hdd 2 --hdd-bytes 1G) ;;
+  swap)
+    TOTAL_MEM=$(grep MemTotal /proc/meminfo | awk '{print $2*1024}')
+    ARGS=(--vm 1 --vm-bytes "${TOTAL_MEM}b" --mmap 1 --mmap-bytes 512M --page-in) ;;
+  net)
+    ARGS=(--udp 2 --udp-port 50000) ;;
   *)
     echo "Unknown class: $CLASS"
     exit 1 ;;
 esac
-
-exec "$STRESS_CMD" "${ARGS[@]}" -t "${DURATION}s" --metrics-brief
